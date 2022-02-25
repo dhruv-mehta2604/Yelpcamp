9f73c447c73101cc1516f1ec29cea19cf2e3dc39
Dhruv Mehta <dhruvmehta870@gmail.com>
2022-02-25 21:37:55 +0530

    some final touches

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
+const MongoDBStore = require("connect-mongo")(session);
+
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(process.env.DB_URL, {
